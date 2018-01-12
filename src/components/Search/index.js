0fa45833e583e5d5059001dffd9aa01346fbbeca
Liam Morgan <lite@getignited.co.uk>
2018-01-12 09:48:33 +0000

    Removed OpenWeatherMap city list json

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,7 @@
 import React     from 'react'
 import PropTypes from 'prop-types'
 
-import CityList from './../../data/city.list.json'
+//import CityList from './../../data/city.list.json'
 
 
 export default class Search extends Component {
